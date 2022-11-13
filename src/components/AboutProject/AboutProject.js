6413d4d0f420e01467c4c8d7498c83215db0bb96
melodoc <liubov_rz@yahoo.com>
2022-11-14 02:58:44 +0400

    feat. Add about component #2

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -2,8 +2,38 @@ import './AboutProject.css';
 
 export function AboutProject() {
   return (
-    <div className="aboutProject">
-        компонент с описанием дипломного проекта
-    </div>
+    <section className="about">
+      <h2 className="about__headline">О проекте</h2>
+      <ul className="about__list about__steps">
+        <li className="about__item">
+          <h3 className="about__title">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <p className="about__subtitle">
+            Составление плана, работу над бэкендом, вёрстку,
+            добавление функциональности и финальные доработки.
+          </p>
+        </li>
+        <li className="about__item">
+          <h3 className="about__title">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className="about__subtitle">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые
+            нужно было соблюдать, чтобы успешно защититься.
+          </p>
+        </li>
+      </ul>
+      <ul className="about__list about__time">
+        <li className="about__time-item">
+          <p className="about__time-period about__time-period-bright">1 неделя</p>
+          <p className="about__time-description">Back-end</p>
+        </li>
+        <li className="about__time-item">
+          <p className="about__time-period">4 недели</p>
+          <p className="about__time-description">Front-end</p>
+        </li>
+      </ul>
+    </section>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,5 @@
 import { Header } from '../Header/Header';
 import { Promo } from '../Promo/Promo';
-import { NavTab } from '../NavTab/NavTab';
 import { AboutProject } from '../AboutProject/AboutProject';
 import { Techs } from '../Techs/Techs';
 import { AboutMe } from '../AboutMe/AboutMe';
@@ -14,7 +13,6 @@ export function Main() {
     <div className='main'>
       <Header/>
       <Promo/>
-      <NavTab/>
       <AboutProject/>
       <Techs/>
       <AboutMe/>

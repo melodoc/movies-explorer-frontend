580dc63741a616f6964fb5e2fd96ef4289afc4d3
melodoc <liubov_rz@yahoo.com>
2022-11-15 02:50:01 +0400

    feat. Add aboutMe component #2

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,9 +1,37 @@
+import { Headline } from '../../shared-components/headline/Headline';
+import profile from '../../images/pic.png';
+
 import './AboutMe.css';
 
 export function AboutMe() {
   return (
-    <div className="aboutMe">
-        компонент с информацией о студенте
-    </div>
+    <section className="about-me">
+      <Headline text="Студент" />
+      <ul className="about-me__list">
+        <li className="about-me__list-item">
+          <p className="about-me__title">Виталий</p>
+          <p className="about-me__subtitle">
+            Фронтенд-разработчик, 30 лет
+          </p>
+          <p className="about-me__description">
+            Я родился и живу в Саратове, закончил факультет экономики
+            СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а
+            ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года
+            работал в компании «СКБ Контур». После того, как прошёл
+            курс по веб-разработке, начал заниматься фриланс-заказами
+            и ушёл с постоянной работы.
+          </p>
+          <a
+            className="about-me__link"
+            href="https://github.com/melodoc/"
+          >
+            Github
+          </a>
+        </li>
+        <li className="about-me__list-item">
+          <img className="about-me__image" src={profile} alt="profile" />
+        </li>
+      </ul>
+    </section>
   );
 }

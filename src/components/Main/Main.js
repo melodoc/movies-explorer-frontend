3a5440f9f65dde99de9cc0293c1b9dff768a1fd8
melodoc <liubov_rz@yahoo.com>
2022-11-23 22:46:02 +0400

    feat. Change mocked data for movies #2

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -11,6 +11,52 @@ import './Main.css';
 
 export function Main() {
   const isBanner = false;
+  const isSaved = true;
+
+  const cards = [
+    {
+      src: 'https://pm1.narvii.com/7283/db4336ada8dadfd88c83ed5111304b5f0d6eafa0r1-728-1097v2_hq.jpg',
+      label: '33 слова о дизайне',
+      duration: '1ч 17м',
+      isSaved: true
+    },
+    {
+      src: 'https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?w=2000',
+      label: 'Бег это свобода',
+      duration: '1ч 17м',
+    },
+    {
+      src: 'https://klike.net/uploads/posts/2020-11/1605600161_25.jpg',
+      label: 'Gimme Danger: История Игги и The Stooges',
+      duration: '1ч 17м'
+    }
+  ];
+
+  const mockedCards = new Array(5)
+    .fill(null)
+    .map(() => [...cards])
+    .flat();
+
+  const savedCards = [
+    {
+      src: 'https://pm1.narvii.com/7283/db4336ada8dadfd88c83ed5111304b5f0d6eafa0r1-728-1097v2_hq.jpg',
+      label: '33 слова о дизайне',
+      duration: '1ч 17м',
+      hasDeleteBtn: true
+    },
+    {
+      src: 'https://img.freepik.com/free-photo/pink-sky-background-with-crescent-moon_53876-129048.jpg?w=2000',
+      label: 'Бег это свобода',
+      duration: '1ч 17м',
+      hasDeleteBtn: true
+    },
+    {
+      src: 'https://klike.net/uploads/posts/2020-11/1605600161_25.jpg',
+      label: 'Gimme Danger: История Игги и The Stooges',
+      duration: '1ч 17м',
+      hasDeleteBtn: true
+    }
+  ];
 
   return isBanner ? (
     <>
@@ -23,10 +69,16 @@ export function Main() {
       <Portfolio />
       <Footer />
     </>
+  ) : isSaved ? (
+    <>
+      <Header type={headerType.Main} isLoggedIn={true} />
+      <Movies cards={savedCards} />
+      <Footer />
+    </>
   ) : (
     <>
       <Header type={headerType.Main} isLoggedIn={true} />
-      <Movies />
+      <Movies cards={mockedCards} />
       <Footer />
     </>
   );

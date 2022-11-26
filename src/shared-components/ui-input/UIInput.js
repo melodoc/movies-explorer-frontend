cfc7d5596ebda4c50b629903f6f53235027c8d2c
melodoc <liubov_rz@yahoo.com>
2022-11-26 18:59:35 +0400

    feat. Add input errors #4

--- src/shared-components/ui-input/UIInput.js
+++ src/shared-components/ui-input/UIInput.js
@@ -1,8 +1,29 @@
+import { useRef, useState } from 'react';
 import { INPUT_TYPES } from '../../constants/inputTypes';
 
 import './UIInput.css';
 
-export function UIInput({ label, type, value, required, handleChange }) {
+export function UIInput({
+  label,
+  type,
+  value,
+  required,
+  handleChange
+}) {
+  const input = useRef(null);
+  const [inputState, setInputState] = useState({
+    valid: true,
+    text: ''
+  });
+
+  const checkValidation = () => {
+    handleChange && handleChange();
+    setInputState({
+      valid: input.current.validity.valid,
+      text: input.current.validationMessage
+    });
+  };
+
   return (
     <div className="field__container">
       <label className="field__label" htmlFor={label}>
@@ -10,7 +31,7 @@ export function UIInput({ label, type, value, required, handleChange }) {
       </label>
       <input
         className="field__input"
-        onChange={handleChange}
+        onChange={checkValidation}
         type={type}
         id={label}
         placeholder={label}
@@ -19,7 +40,9 @@ export function UIInput({ label, type, value, required, handleChange }) {
         required={required}
         minLength={2}
         maxLength={type === INPUT_TYPES.Email ? 200 : 40}
+        ref={input}
       />
+      {!inputState.valid && <p className="field__valid-text">{inputState.text}</p>}
     </div>
   );
 }
